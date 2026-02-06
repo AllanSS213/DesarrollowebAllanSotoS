f0f176c33ddd3f5b6105ae84302c4d446611976f
AllanSS213 <allansotosuarez070@gmail.com>
2026-02-05 19:45:15 -0600

    Add files via upload

---
+++ practica/Practica01/src/main/java/com/practica/practica/Practica1AllanSotoApplication.java
@@ -0,0 +1,13 @@
+package com.practica.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1AllanSotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1AllanSotoApplication.class, args);
+	}
+
+}
---
+++ practica/Practica01/src/test/java/com/practica/practica/Practica1AllanSotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1AllanSotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
